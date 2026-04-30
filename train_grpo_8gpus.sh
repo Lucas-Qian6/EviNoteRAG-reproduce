cfa722403ad37658cc04c5b6cdec53dbd0f3ce4a
qianyujun <yq2432@columbia.edu>
2026-04-30 11:03:36 -0400

    improve claim prompt

--- train_grpo_8gpus.sh
+++ train_grpo_8gpus.sh
@@ -10,7 +10,7 @@ python3 -m ray.scripts.scripts start --head
 
 export BASE_MODEL='/mnt/finder/qyj/models/Qwen2.5-7B-Instruct'
 WAND_PROJECT='EviNoteRAG'
-EXPERIMENT_NAME='0429_new'
+EXPERIMENT_NAME='0430_step30'
 
 # Resume actor weights from a previous checkpoint.
 # BASE_MODEL must remain the original model because it is used as the ref policy.
